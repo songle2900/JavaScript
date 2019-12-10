60a2c9cef53fd77fdd53f93abc2ee39a1ca1cdd4
Young Min Song <ysong88@myseneca.ca>
2019-12-09 21:26:38 -0500

    Testing index

---
+++ Practice/index.js
@@ -0,0 +1,70 @@
+// const eric = {
+//   name: "Eric",
+//   age: 28,
+//   gender: "Male",
+//   favMov: ["Lord of the Ring", "Avengers", "Iron Man"],
+//   family: [
+//     {
+//       name: "Carol",
+//       status: "girlfriend",
+//       family1: ["Josh", "Jessica"]
+//     },
+//     {
+//       name: "Kwangseok",
+//       status: "Dad"
+//     }
+//   ]
+// };
+
+// console.log("Who is carol's brother: " + eric.family[0].family1[0]);
+// console.log("What is Eric's gender: " + eric.gender);
+// console.log("Carol is Eric's " + eric.family[0].status);
+// console.log(
+//   eric.family[1].name + " is " + eric.name + "'s " + eric.family[1].status
+// );
+
+// // 1
+// const func = x => {
+//   if (x < 5) {
+//     const func1 = y => {
+//       for (let i = 1; i <= y; i++) {
+//         console.log("hello!");
+//       }
+//     };
+//     func1(x);
+//   }
+// };
+// func(4);
+
+// // 2
+// const func2 = x => `Hello, ${x}`;
+// func2("Eric");
+
+// // 3
+// const arr = ["Eric", "Carol", "Henry"];
+
+// const func3 = name => {
+//   for (let i = 0; i < name.length; i++) {
+//     console.log("Hello, " + name[i]);
+//   }
+// };
+// func3(arr);
+
+// const greetEric = (name, age) => `Hello, ${name}! You are ${age} years old`;
+
+// console.log(greetEric("Eric", 28));
+
+// const calculator = {
+//     plus: (x, y) => {
+//         return x + y;
+//     }
+// }
+
+// //           console.log(greetEric);
+// const plus = calculator.plus(5, 5);
+
+const arr = [1, 2, 3, 4, 5, 6, 7]
+
+const arr2 = arr.map((item) => {
+    
+});
---
+++ Practice/note.js
@@ -0,0 +1,18 @@
+// String 객체
+
+// 1. charAt()
+// 문자열에서 인자로 넘긴 index에 해당하는 문자형 데이터를 반환해주는 함수
+const a = "Hello";
+const b = a.charAt(1);
+
+console.log(b); // e
+
+// 2. indexOf() / lastIndexOf()
+// indexOf()
+
+const x = "eric";
+console.log(x);
+const y = [...x];
+console.log(y);
+const z = y.join('')
+console.log(z);
