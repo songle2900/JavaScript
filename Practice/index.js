13d6545cb5d5781389131b5bd146a78687b60870
Young Min Song <ysong88@myseneca.ca>
2020-02-28 17:20:45 -0500

    Array, Object built-in practice

--- Practice/index.js
+++ Practice/index.js
@@ -7,6 +7,7 @@
 
 
 
+
 // --- Example
 //   fizzBuzz(5);
 //   1
@@ -14,8 +15,21 @@
 //   fizz
 //   4
 //   buzz
+const fizzBuzz = (n) => {
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else if (i % 3 === 0) {
+            console.log('Fizz');
+        } else {
+            console.log(i);
+        };
+    };
+};
 
-
+fizzBuzz(15);
 
 // --- Examples
 // maxChar("abcccccccd") === "c"
