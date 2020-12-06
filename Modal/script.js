1cb758892bc679f72db4e1e4d9c63ff50098bb56
Eric Song <eric@thinkingcap.com>
2020-12-06 01:30:55 -0500

    Modal Updated

---
+++ Modal/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const closeFunc = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openFunc = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openFunc);
+}
+
+btnCloseModal.addEventListener('click', closeFunc);
+overlay.addEventListener('click', closeFunc);
