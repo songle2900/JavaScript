97e060290b0dab29bdc72c3aa82d73fb770b86fe
Eric Song <eric@thinkingcap.com>
2020-12-12 19:26:02 -0500

    Keypressed

--- Modal/script.js
+++ Modal/script.js
@@ -1,23 +1,30 @@
-'use strict';
+"use strict";
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 const closeFunc = () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
 const openFunc = () => {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', openFunc);
+  btnsOpenModal[i].addEventListener("click", openFunc);
 }
 
-btnCloseModal.addEventListener('click', closeFunc);
-overlay.addEventListener('click', closeFunc);
+btnCloseModal.addEventListener("click", closeFunc);
+overlay.addEventListener("click", closeFunc);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+});
