bd4efb9a17cfd43afed42e03ef83472ea9265385
Eric Song <dudals2900@gmail.com>
2020-12-28 20:25:10 -0500

    test updated

--- Modal/script.js
+++ Modal/script.js
@@ -23,9 +23,8 @@ btnCloseModal.addEventListener("click", closeFunc);
 overlay.addEventListener("click", closeFunc);
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
-    if (!modal.classList.contains("hidden")) {
-      closeFunc();
-    }
+  // check if modal has hidden class
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeFunc();
   }
 });
